263dfeb7b08667c58ac1738dd81681df3eb71d06
abigaillealperez <lealperezabigail.alp@gmail.com>
2020-01-29 19:57:05 -0500

    Create Form1.Designer.cs

---
+++ Chapter07ProgramTableOfSquares/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Chapter07ProgramTableOfSquares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblNumbersquared = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStart.Location = new System.Drawing.Point(21, 20);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(120, 20);
+            this.lblStart.TabIndex = 0;
+            this.lblStart.Text = "Starting table value:";
+            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEnd.Location = new System.Drawing.Point(21, 49);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(120, 20);
+            this.lblEnd.TabIndex = 1;
+            this.lblEnd.Text = "Ending table value:";
+            this.lblEnd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(21, 90);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(65, 20);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(92, 90);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(65, 20);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(169, 90);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(65, 20);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumber.Location = new System.Drawing.Point(21, 127);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(86, 20);
+            this.lblNumber.TabIndex = 5;
+            this.lblNumber.Text = "Number";
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // lblNumbersquared
+            // 
+            this.lblNumbersquared.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumbersquared.Location = new System.Drawing.Point(113, 127);
+            this.lblNumbersquared.Name = "lblNumbersquared";
+            this.lblNumbersquared.Size = new System.Drawing.Size(121, 20);
+            this.lblNumbersquared.TabIndex = 6;
+            this.lblNumbersquared.Text = "Number Squared";
+            this.lblNumbersquared.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(154, 50);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(80, 20);
+            this.txtEnd.TabIndex = 7;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(154, 20);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(80, 20);
+            this.txtStart.TabIndex = 8;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(21, 163);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.ScrollAlwaysVisible = true;
+            this.lstOutput.Size = new System.Drawing.Size(213, 134);
+            this.lstOutput.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 320);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.lblNumbersquared);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblNumbersquared;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
